6bde0b5a70cb64de78e4f561928af0bfcdbd4f18
Joemarie <jojodenoy@gmail.com>
2023-09-21 13:36:59 +0800

    Complete step 5: Add a Display method to the Job class

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -5,6 +5,12 @@ public class Job
     public string _company;
     public string _jobTitle;
     public int _startYear;
-    public int _endyear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
 
 }
